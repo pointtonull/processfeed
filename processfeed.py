9c8cad1bc60aedcdbb25540f5b4cbdf7a92be07a
Carlos Cabrera <point_to_null@esdebian.org>
2011-04-23 07:03:01 -0300

    Complete rewrite xD

--- processfeed.py
+++ processfeed.py
@@ -1,62 +1,44 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 
 from ConfigParser import SafeConfigParser
+from collections import defaultdict
 from optparse import OptionParser, OptionValueError
 from subprocess import Popen, PIPE
-from collections import defaultdict
 import feedparser
+import logging
 import os
 import re
 import sys
 
-LOGFILE = os.path.expanduser("~/.processfeed.log")
-CONFIGFILE = os.path.expanduser('~/.processfeed')
+try:
+    from collections import OrderedDict
+except ImportError:
+    from odict import odict as OrderedDict
 
-class Verbose:
-    def __init__(self, verbosity, prefix="", ident=True):
-        self.verbosity = False if verbosity < 0 else True
-        self.prefix = prefix
-        self.ident = ident
+APP_NAME = "processfeed"
+LOG_FILE = os.path.expanduser("~/.%s.log" % APP_NAME)
+CONFIG_FILES = [os.path.expanduser("~/.%s" % APP_NAME),
+    os.path.expanduser("~/%s.ini" % APP_NAME)]
+VERBOSE = 20
 
-    def __call__(self, *args):
-        if self.verbosity:
-            message = " ".join((unicode(e) for e in args))
-            sys.stderr.write("%s%s%s\n" % ("  " * self.get_depth(), self.prefix,
-                message))
 
-    def get_depth(self):
-        if not self.ident:
-            return 0
-        else:
-            def exist_frame(n):
-                try:
-                    if sys._getframe(n):
-                        return True
-                except ValueError:
-                    return False
-
-            now = 0
-            maxn = 1
-            minn = 0
-
-            while exist_frame(maxn):
-                minn = maxn
-                maxn *= 2
-
-            # minn =< depth < maxn
-            middle = (minn + maxn) / 2
-          
-            while minn < middle:
-                if exist_frame(middle):
-                    minn = middle
-                else:
-                    maxn = middle
-
-                middle = (minn + maxn) / 2
-          
-            return max(minn - 3, 0) #4 == len(main, Verbose, get_depth)
-
+#@builtin
+#def run(command, stdin=""):
+#    if stdin:
+#        debug("""Starting "%s" with stdin pipe""" % command)
+#        proc = Popen(command, shell=True, stdin=PIPE)
+#        proc.stdin.write(stdin.encode("UTF-8", "replace"))
+#        proc.stdin.close()
+#        proc.wait()
+#    else:
+#        debug("""Starting "%s" with stdin pipe""" % command)
+#        proc = Popen(command, shell=True)
+#        proc.wait()
+#    if proc.returncode:
+#        warning("""Process '%s' exited with error %d""" % (command,
+#            proc.returncode))
+#    return proc.returncode
 
 
 def write(text, destination):
@@ -64,6 +46,42 @@ def write(text, destination):
     f = open(destination, "a")
     f.write("%s\n" % text.encode("UTF-8", "replace"))
     f.close()
+
+
+def get_depth():
+    def exist_frame(n):
+        try:
+            if sys._getframe(n):
+                return True
+        except ValueError:
+            return False
+
+    now = 0
+    maxn = 1
+    minn = 0
+
+    while exist_frame(maxn):
+        minn = maxn
+        maxn *= 2
+
+    middle = (minn + maxn) / 2
+  
+    while minn < middle:
+        if exist_frame(middle):
+            minn = middle
+        else:
+            maxn = middle
+
+        middle = (minn + maxn) / 2
+  
+    return max(minn - 2, 0) #4 == len(main, get_depth)
+
+
+def ident(func, identation="  "):
+    def decorated(message, *args, **kwargs):
+        newmessage = "%s%s" % (identation * (get_depth() - 1), message)
+        return func(newmessage, *args, **kwargs)
+    return decorated
 
 
 def read(filename):
@@ -77,109 +95,110 @@ def read(filename):
     return lines
 
 
-def get_id(section, entry):
-    for key in ("id", "link"):
-        if key in entry:
-            idstring = entry[key]
-            break
+class Processfeed(object):
 
-    else:
-        error("Entry cannot be identified, please report this bug."
-            "Dump:\n%s" % entry.keys())
+    def __init__(self, config):
+        self.config = config
 
-    return "%s::%s" % (section, idstring)
+        sections = config.sections()
+        self.actions = OrderedDict()
+        for section in sections:
+            if section.startswith("ACTION"):
+                self.actions[section] = OrderedDict()
+                self.actions[section]["_name"] = section[7:]
+                for key, value in config.items(section, True):
+                    self.actions[section][key] = value
+                debug(self.actions[section])
 
-
-def run(command, stdin=""):
-    """Execute the given command and pases stdin (if any) to the proc.
-    """
-    if stdin:
-        debug("""Starting "%s" with stdin pipe""" % command)
-        proc = Popen(command, shell=True, stdin=PIPE)
-        proc.stdin.write(stdin.encode("UTF-8", "replace"))
-        proc.stdin.close()
-        proc.wait()
-    else:
-        debug("""Starting "%s" with stdin pipe""" % command)
-        proc = Popen(command, shell=True)
-        proc.wait()
-
-    if proc.returncode:
-        warning("""Process '%s' exited with error %d""" % (command,
-            proc.returncode))
-    return proc.returncode
-
-
-def process_section(section):
-    info("Processing %s" % section)
-    items = defaultdict(lambda: u"None")
-    for key, value in config.items(section, True):
-        items[key] = value.decode("utf-8")
-
-    enabled = items["enabled"]
-
-    if enabled == "False":
-        debug("Enabled == False")
-        return
-
-    feed = feedparser.parse(items["feed"])
-
-    if "bozo_exception" in feed:
-        error("%s %s" % (items["feed"], feed["bozo_exception"]))
-        return
-    else:
-        info("  Readed %d entries" % len(feed["entries"]))
-
-    for entry in feed["entries"][::-1]:
-        process = False
-        idstring = unicode(get_id(section, entry))
-
-        if idstring in history:
-            debug("%s in history" % idstring)
+        self.history = read(LOG_FILE)
+    
+    
+    def get_entry_id(self, action, entry):
+        for key in ("id", "link"):
+            if key in entry:
+                idstring = entry[key]
+                break
         else:
-            info("  %s:" % idstring)
-            safe_globals = {"re": re, "feed": feed, "entry": entry}
-            enabled = eval(items["enabled"], safe_globals)
+            error("Entry cannot be identified, please report this bug."
+                "Dump:\n%s" % entry.keys())
+        return "%s::%s" % (action["_name"], idstring)
 
-            if enabled:
-                info("  Enabled: %s" % enabled)
 
-                info("  Command code: %s" % items["command"])
-                command = unicode(eval(items["command"], safe_globals))
-                info("  Command text: " + command.encode("ascii", "replace"))
+    def get_entries(self, feed):
+        feed = feedparser.parse(feed)
 
-                info("  Stdin code: %s" % items["stdin"].encode("ascii",
-                    "replace"))
-                stdin = unicode(eval(items["stdin"], safe_globals))
-                info("  Stdin text: %s" % stdin.encode("ascii", "replace"))
+        if "bozo_exception" in feed:
+            error("Bozo exception: %s %s"
+                % ("feed", feed["bozo_exception"]))
+            return []
+        else:
+            entries = feed["entries"][::-1]
+            info("Readed %d entries" % len(entries))
+            return entries
 
-                exitcode = run(command, stdin)
-                write("%s #%s" % (idstring, entry["title"]), LOGFILE)
-                return exitcode
 
+    def get_news(self, action, entries=None):
+        if not entries:
+            entries = self.get_entries(action["feed"])
+
+        new_entries = []
+        for entry in entries:
+            idstring = self.get_entry_id(action, entry)
+            if idstring in self.history:
+                debug("%s in history" % idstring)
             else:
-                info("  Not enabled: %s" % enabled)
-                write("%s #%s (OMITED)" % (idstring, entry["title"]),
-                    LOGFILE)
+                new_entries.append(entry)
+            
+        return new_entries
+
+
+    def process_entry(self, action, entry):
+        idstring = self.get_entry_id(action, entry)
+        debug(idstring)
+
+        safe_globals = {}
+        safe_globals["__builtins__"] = globals()["__builtins__"]
+        safe_globals["re"] = re
+
+        safe_locals = {}
+        safe_locals["entry"] = entry
+
+        result = None
+        del(action["feed"])
+        for name, expression in action.iteritems():
+            debug("%s = %s" % (name, expression))
+            if not name.startswith("_"):
+                result = eval(expression, safe_globals, safe_locals)
+                safe_locals[name] = result
+            else:
+                safe_locals[name] = expression
+
+            moreinfo("%s = %s" % (name, result))
+            debug("safe_locals: %s" % safe_locals)
+
+            if name.startswith("assert"):
+                if not safe_locals[name]:
+                    info("Ignored because assert is false.")
+                    return
+
+        write("%s #%s" % (idstring, entry["title"]), LOG_FILE)
+        return result
+
+
+    def process_action(self, action):
+        new_entries = self.get_news(action)
+        if new_entries:
+            return self.process_entry(action, new_entries[0])
+
+
+    def process_all(self):
+        for name, action in self.actions.iteritems():
+            moreinfo(name)
+            debug(action)
+            self.process_action(action)
 
 
 def get_options():
-    def define_variable(option, opt_str, value, parser):
-        """Handle the -d/--define option and populate the variables dict"""
-        logging.debug(option.dest)
-        logging.debug(value)
-        variables = getattr(parser.values, option.dest)
-
-        try:
-            variable = re.search(r"".join(("^\s*([a-zA-Z_][a-zA-Z\d_]*)",
-                "\s*=\s*(.*)\s*$")), value).groups()
-        except AttributeError:
-            raise OptionValueError("Declaración incorrecta: %s" % value)
-        else:
-            variables.update((variable,))
-
-        logging.debug(variables)
-
     # Instance the parser and define the usage message
     optparser = OptionParser(usage="""
     %prog [-vqdsc]""", version="%prog .2")
@@ -195,45 +214,67 @@ def get_options():
         help="Increment verbosity")
     optparser.add_option("-q", "--quiet", action="count", dest="quiet",
         help="Decrement verbosity")
-    optparser.add_option("-d", metavar="VAR=VALUE", action="callback",
-        callback=define_variable, type="string", nargs=1, dest="variables",
-        help="Define a variable VAR to VALUE")
 
     # Define the default options
-    optparser.set_defaults(verbose=0, quiet=0, variables={},
-        conffile=CONFIGFILE, )
+    # Define the default options
+    optparser.set_defaults(verbose=0, quiet=0, logfile=LOG_FILE,
+        conffile="")
 
     # Process the options
     return optparser.parse_args()
 
 
-def process():
-    """Read the feed and execute the defined rules
-    """
+def process(config):
+    """Read the feed and execute the defined rules"""
 
     sections = config.sections()
     debug("Sections: %s" % sections)
 
     for section in sections:
-        process_section(section)
+        process_section(config, section)
+
+
+def get_config(conf_file=None):
+    'Read config files'
+    config = SafeConfigParser(None, OrderedDict)
+    read_from = conf_file or CONFIG_FILES
+    files = config.read(read_from)
+    debug("Readed config files at: %s" % files)
+
+    return config
+
+
+def main(options, args):
+    """The main routine"""
+    # Read the config values from the config files
+    config = get_config(options.conffile)
+    processfeed = Processfeed(config)
+    processfeed.process_all()
+    return
 
 
 if __name__ == "__main__":
     # == Reading the options of the execution ==
     options, args = get_options()
 
-    error = Verbose(options.verbose - options.quiet + 1, "E: ")
-    info = Verbose(options.verbose - options.quiet + 0)
-    warning = Verbose(options.verbose - options.quiet - 1, "W: ")
-    debug = Verbose(options.verbose - options.quiet - 2, "D: ")
+    format = "%(asctime)s - %(message)s"
+    logging.basicConfig(format=format, level=0)
+#    logging.basicConfig(format=format, filename=LOG_FILE, level=0)
+    logger = logging.getLogger()
+    logger.handlers[0].setLevel(VERBOSE)
 
+    VERBOSE = (options.quiet - options.verbose) * 10 + 30
+    stderr = logging.StreamHandler()
+    stderr.setLevel(VERBOSE)
+    logger.addHandler(stderr)
+
+    debug = ident(logger.debug)
+    moreinfo = ident(logger.info)
+    info = ident(logger.warning) # Default
+    warning = ident(logger.error)
+    error = ident(logger.critical)
+
+    debug("Verbose level: %s" % VERBOSE)
     debug("""Options: '%s', args: '%s'""" % (options, args))
 
-    # Read the values from the file
-    config = SafeConfigParser()
-    config.read(options.conffile)
-
-    # Read the history log
-    history = read(LOGFILE)
-
-    exit(process())
+    exit(main(options, args))
